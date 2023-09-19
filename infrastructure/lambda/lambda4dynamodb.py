5f3e15c3f0afb139072b58582207f5f9164928b6
heonjaes <heonjae.shin00@gmail.com>
2023-09-19 16:48:58 +1000

    Changes to lambda function for testing purposes

--- infrastructure/lambda/lambda4dynamodb.py
+++ infrastructure/lambda/lambda4dynamodb.py
@@ -4,9 +4,10 @@ import boto3
 
 os.environ['AWS_DEFAULT_REGION'] = 'ap-southeast-2'
 dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('resume-count-table')
 
-def lambda_handler(event, context):
-    table = dynamodb.Table('resume-count-table')
+
+def lambda_handler(event, context, table):
 
     response = table.get_item(Key={
         'ID':'0'
