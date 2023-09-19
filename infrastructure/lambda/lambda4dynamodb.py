941ea22f277dc99fb4e7b93d180fafa2e7ab201a
heonjaes <heonjae.shin00@gmail.com>
2023-09-19 21:45:06 +1000

    Update lambda function

--- infrastructure/lambda/lambda4dynamodb.py
+++ infrastructure/lambda/lambda4dynamodb.py
@@ -1,14 +1,18 @@
 import json
 import boto3
+import os
+
+os.environ['AWS_DEFAULT_REGION'] = 'ap-southeast-2'
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('resume-count-table')
-def lambda_handler(event, context):
+
+def lambda_handler(event, context, table_name = 'resume-count-table'):
+    table = dynamodb.Table(table_name)
+
     response = table.get_item(Key={
         'ID':'0'
     })
     views = response['Item']['visit_count']
     views = views + 1
-    print(views)
     response = table.put_item(Item={
             'ID':'0',
             'visit_count': views
